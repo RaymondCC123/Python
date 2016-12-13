c4531a7dc435c1ad2c988e0fe91833e8ea572b8c
RaymondCC123 <caocheng1994@gmail.com>
2016-12-13 18:28:53 +0100

    Add files via upload

--- kickGame/kickGame.py
+++ kickGame/kickGame.py
@@ -1,1 +1,25 @@
+from random import choice
+score=[0,0]
 
+def kcik():
+    for i in range(0,5):
+        print'left,centre,right'
+        you=raw_input()
+        print'you choose '+you
+        direction=['left','centre','right']
+        p=choice(direction)
+        print 'computer saved '+p
+        if you==p:
+            print 'what a pity!!!'
+            score[1]+=1
+        else:
+            print'goal!!!'
+            score[0]+=1
+    print '\n'
+    print 'score of you is '+str(score[0])
+    print 'score of pc is '+str(score[1])
+
+print score
+
+while score[0]==score[1]:
+    kcik()
