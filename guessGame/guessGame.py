f735ab48108969776f7f05ceefe54a24b365b967
RaymondCC123 <caocheng1994@gmail.com>
2016-12-21 12:03:39 +0100

    Add files via upload

--- guessGame/guessGame.py
+++ guessGame/guessGame.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from random import randint
 
-
+name=raw_input()
 f=file('game.txt','r')
-score=f.read().split(' ')
+lines=f.readlines()
+scores={}
+for i in lines:
+    s=i.split(' ')
+    scores[s[0]]=s[1:]
+score=scores.get(name)
+if score==None :
+    score=[0,0,0]
+
+#score=f.read().split(' ')
 game_times=int(score[0])
 min_times = int(score[1])
 total_times = int(score[2])
@@ -36,7 +45,13 @@ if game_times==0 or times<min_times:
 total_times+=times#把本次轮数加到游戏总轮数里：
 game_times+=1#把游戏次数加1
 
-result='%d %d %d' %(game_times, min_times, total_times)
+scores[name]=[str(game_times), str(min_times), str(total_times)]
+
+#把scores中的每一项按照“名字 游戏次数 最低轮数 总轮数\n”的格式拼成字符串，再全部放到result里
+result=''
+for i in scores:
+    line=i+' '+' '.join(scores[i])+'\n'
+    result+=line
 
 f=file('game.txt','w')
 f.write(result)
