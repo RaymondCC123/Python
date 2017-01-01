af010edec8ed0013de6f4a5ae5cf834925e014d1
RaymondCC123 <caocheng1994@gmail.com>
2017-01-01 16:55:12 +0100

    Add files via upload

---
+++ weather/get_cityID.py
@@ -0,0 +1,44 @@
+import urllib2
+
+url1 = 'http://m.weather.com.cn/data5/city.xml'
+content1 = urllib2.urlopen(url1).read()
+#print content1
+provinces = content1.split(',') #split elements
+#print provinces[1]
+
+eles = []
+cities = []
+result = {}
+#result = 'city = {\n'
+
+url = 'http://m.weather.com.cn/data3/city%s.xml'# grab city_info in each province
+for i in provinces[1:2]:
+    eles = i.split('|')
+    url2 = url % (eles[0])
+    content2 = urllib2.urlopen(url2).read()
+    #print content2
+    cities = content2.split(',')
+
+    for c in cities:# grap district info in each city
+        eles = c.split('|')
+        url3 = url % (eles[0])
+        content3 = urllib2.urlopen(url3).read()
+        print content3
+        districts = content3.split(',')
+
+        for d in districts:# grap the exact code of each district
+            eles = d.split('|')
+            name = eles[1]
+            url4 = url % eles[0]
+            content4 = urllib2.urlopen(url4).read()
+            #print content4
+            code = content4.split('|')[1]
+            result[name] = code
+            #line = "'%s': '%s',\n" %(name,code)
+            print name
+            #result += line
+            print result
+
+f = file('ville.py', 'w')
+f.write('ville = ' + str(result))
+f.close()
---
+++ weather/weather_china.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import json
+import city
+import ville
+import urllib2
+
+cityname = raw_input('Whose temperature you want to know ？\n')
+citycode = ville.ville.get(cityname)
+#citycode = city.city.get(cityname)
+if citycode:
+    try:
+        url = ('http://www.weather.com.cn/data/cityinfo/%s.html' % (citycode))
+        content = urllib2.urlopen(url).read()
+        print content
+        data = json.loads(content)
+        #print type(content)
+        #print type(data)
+        result = data['weatherinfo']
+        str_temp = '%s\n%s ~ %s' % (result['weather'], result['temp1'], result['temp2'])
+        print str_temp
+    except:
+        print 'search failed'
+else:
+    print 'city not existed'
+
+
